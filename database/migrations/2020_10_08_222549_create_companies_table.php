88173fe8f26c17836c339b717e628279adc44a61
Juan Fernando Hp <informacion.smartpc@gmail.com>
2020-10-08 22:28:59 -0500

    create model and migration for companies

---
+++ database/migrations/2020_10_08_222549_create_companies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('phone', 12)->nullable();
+            $table->string('email', 65)->nullable();
+            $table->string('address', 150)->nullable();
+            $table->string('logo', 45)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
